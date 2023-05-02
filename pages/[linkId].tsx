45cd35d8dba01fa85e152032d720b13f733b0711
Mukhammadyusuf Abdurakhimov <papa@uwussi.moe>
2023-05-02 01:34:20 -0700

    UI Update/Some more features

--- pages/[linkId].tsx
+++ pages/[linkId].tsx
@@ -1,42 +1,38 @@
 import React from 'react'
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 type Params = {
-	params: {
-		linkId: string
-	}
+  params: {
+    linkId: string
+  }
 }
 
-export const getServerSideProps = async ({params}: Params) => {
+export const getServerSideProps = async ({ params }: Params) => {
+  try {
+    const { linkId } = params
+    const url = await prisma.link.findMany({ where: { linkId } })
 
-    try {
-        const { linkId } = params;
-        const url = await prisma.link.findMany({where: { linkId }});
-
-        if(url.length !== 0) {        
-            return {
-                redirect: {
-                    destination: url[0].url
-                },
-            }
-        } else {
-            return {
-                redirect: {
-                    destination: "/"
-                },
-            }
-        }
-    } catch (error) {
-        console.log(error);
+    if (url.length !== 0) {
+      return {
+        redirect: {
+          destination: url[0].url,
+        },
+      }
+    } else {
+      return {
+        redirect: {
+          destination: '/',
+        },
+      }
     }
-
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const Link = () => {
-  return (
-    <div>Link</div>
-  )
+  return <div>Link</div>
 }
 
 export default Link
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import React, { useState } from 'react';
-import AddLink from '../components/AddLink';
+import React, { useState } from 'react'
+import AddLink from '../components/AddLink'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card'
 
 const Home: NextPage = () => {
+  const [id, setId] = useState('')
 
-  const [id, setId] = useState("");
-
-  const baseUrl = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
+  const baseUrl =
+    typeof window !== 'undefined' && window.location.origin
+      ? window.location.origin
+      : ''
 
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-gray-900">
@@ -16,20 +26,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="max-w-2xl bg-white rounded-md w-full p-6 ">
-
-        <h1 className='font-semibold text-2xl'>🔗 Kusari</h1>
-        <p className='text-gray-500 text-sm'>A simple URL shortener</p>
-        <AddLink id={id} setId={setId} />
-
-        {id && (
-          <div className='bg-fuchsia-50 flex items-center justify-center w-full text-xl text-cyan-800 mt-4 p-3 rounded-sm'>
-            <a href={`${baseUrl}/${id}`} target="_blank">{`${baseUrl}/${id}`}</a>
-          </div>
-        )}
-
-      </div>
-
+      <Card>
+        <CardHeader>
+          <CardTitle>🔗 Kusari</CardTitle>
+          <CardDescription>
+            Kasuri is a fast and user-friendly URL shortener.
+            {/* with personalized shortened links and basic analytics. */}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <AddLink id={id} setId={setId} />
+        </CardContent>
+        <CardFooter>
+          {id && (
+            <div className="bg-fuchsia-50 flex items-center justify-center w-full text-xl text-cyan-800 mt-4 p-3 rounded-sm">
+              <a
+                href={`${baseUrl}/${id}`}
+                target="_blank"
+              >{`${baseUrl}/${id}`}</a>
+            </div>
+          )}
+        </CardFooter>
+      </Card>
     </div>
   )
 }
